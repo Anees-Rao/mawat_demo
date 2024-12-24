990876bfee024c6a4a49dc68b395685d68a68d46
Anees-Rao <aneesengr910@gmail.com>
2024-12-24 10:00:19 +0500

    add-modules

--- src/expert/expert.module.ts
+++ src/expert/expert.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExpertService } from './expert.service';
 import { ExpertController } from './expert.controller';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ExpertController],
   providers: [ExpertService],
 })
